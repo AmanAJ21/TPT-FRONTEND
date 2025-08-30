6e697d9e10630297811acc1d7bf57eab325d3ac8
AmanJ <165487083+AmanAJ21@users.noreply.github.com>
2025-08-30 08:49:28 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { AuthProvider } from "@/lib/auth-context";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
